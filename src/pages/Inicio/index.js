51816668bac5a750a7a71cd53ae4395c48eaef4d
jacsonPolonha <jacsonpolonha@gmail.com>
2024-06-25 05:41:40 -0300

    Adicionado componentes Banner, Titulo, Card e Rodape

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,9 +1,21 @@
+import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
+import Card from "components/Card";
+import Rodape from "components/Rodape";
+import Titulo from "components/Titulo";
 
 
 function Inicio() {
     return (
-        <Cabecalho />
+        <>
+            <Cabecalho />
+            <Banner imagem="home" />
+            <Titulo>
+                <h1>Um lugar para guardar seus videos e filmes!</h1>
+            </Titulo>
+            <Card id='1' titulo="Titulo exemplo" capa='https://thecatapi.com/api/images/get?format=src&type=png' />
+            <Rodape />
+        </>
     )
 }
 
